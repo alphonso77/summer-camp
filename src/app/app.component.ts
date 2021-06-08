99b1afe1d42b4245df468b360b775ecdff58bbf5
alphonso77 <762224+alphonso77@users.noreply.github.com>
2021-06-08 14:53:23 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// will live in global scope
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'summer-camp';
+
+  ngOnInit(){
+    particlesJS.load('particles', 'assets/particles.json', () => {
+      console.log('particles.js config loaded');
+    });
+  }
+
 }
